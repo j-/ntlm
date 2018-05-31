dae87f53c7c7252d36e678f2041c86db8902f103
j- <j@skeoh.com>
2018-05-31 10:23:03 +1000

    Move hash generation into its own module

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,20 +1,5 @@
 import * as React from 'react';
-import { encode } from 'iconv-lite';
-import { createCipheriv } from 'crypto';
-
-const md4 = require('js-md4');
-const TO_ENCODE = 'KGS!@#$%';
-
-function passwordToKey (password: Uint8Array, key: Uint8Array) {
-	key[0] =                        (password[0] >> 0);
-	key[1] = ((password[0]) << 7) | (password[1] >> 1);
-	key[2] = ((password[1]) << 6) | (password[2] >> 2);
-	key[3] = ((password[2]) << 5) | (password[3] >> 3);
-	key[4] = ((password[3]) << 4) | (password[4] >> 4);
-	key[5] = ((password[4]) << 3) | (password[5] >> 5);
-	key[6] = ((password[5]) << 2) | (password[6] >> 6);
-	key[7] = ((password[6]) << 1);
-}
+import { calculatePassNT, calculatePassLM } from './ntlm';
 
 export interface Props {
 
@@ -29,8 +14,8 @@ export interface State {
 export default class Form extends React.Component<Props, State> {
 	state: State = {
 		password: '',
-		passNT: '31D6CFE0D16AE931B73C59D7E0C089C0',
-		passLM: 'AAD3B435B51404EEAAD3B435B51404EE',
+		passNT: calculatePassNT(''),
+		passLM: calculatePassLM(''),
 	};
 
 	render () {
@@ -57,36 +42,14 @@ export default class Form extends React.Component<Props, State> {
 		);
 	}
 
-	private calculateHashes (password: string) {
-		return {
-			passNT: this.calculatePassNT(password),
-			passLM: this.calculatePassLM(password),
-		};
-	}
-
-	private calculatePassNT (password: string) {
-		return md4.hex(encode(password, 'utf-16le')).toUpperCase();
-	}
-
-	private calculatePassLM (password: string) {
-		const buffLM = Buffer.alloc(14);
-		buffLM.write(password.toUpperCase());
-		const key1 = Buffer.alloc(8);
-		passwordToKey(buffLM.slice(0, 7), key1);
-		const key2 = Buffer.alloc(8);
-		passwordToKey(buffLM.slice(7, 14), key2);
-		const cipher1 = createCipheriv('des-ecb', key1, '').update(TO_ENCODE, 'utf8', 'hex').toString();
-		const cipher2 = createCipheriv('des-ecb', key2, '').update(TO_ENCODE, 'utf8', 'hex').toString();
-		return (cipher1 + cipher2).toUpperCase();
-	}
-
 	private handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 	}
 
 	private handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { value: password } = e.currentTarget;
-		const { passNT, passLM } = this.calculateHashes(password);
+		const passNT = calculatePassNT(password);
+		const passLM = calculatePassLM(password);
 		this.setState({ password, passNT, passLM });
 	}
 }
---
+++ src/ntlm.ts
@@ -0,0 +1,32 @@
+import { encode } from 'iconv-lite';
+import { createCipheriv } from 'crypto';
+
+const md4 = require('js-md4');
+const TO_ENCODE = 'KGS!@#$%';
+
+function passwordToKey (password: Uint8Array, key: Uint8Array) {
+	key[0] =                        (password[0] >> 0);
+	key[1] = ((password[0]) << 7) | (password[1] >> 1);
+	key[2] = ((password[1]) << 6) | (password[2] >> 2);
+	key[3] = ((password[2]) << 5) | (password[3] >> 3);
+	key[4] = ((password[3]) << 4) | (password[4] >> 4);
+	key[5] = ((password[4]) << 3) | (password[5] >> 5);
+	key[6] = ((password[5]) << 2) | (password[6] >> 6);
+	key[7] = ((password[6]) << 1);
+}
+
+export function calculatePassNT (password: string) {
+	return md4.hex(encode(password, 'utf-16le')).toUpperCase();
+}
+
+export function calculatePassLM (password: string) {
+	const buffLM = Buffer.alloc(14);
+	buffLM.write(password.toUpperCase());
+	const key1 = Buffer.alloc(8);
+	passwordToKey(buffLM.slice(0, 7), key1);
+	const key2 = Buffer.alloc(8);
+	passwordToKey(buffLM.slice(7, 14), key2);
+	const cipher1 = createCipheriv('des-ecb', key1, '').update(TO_ENCODE, 'utf8', 'hex').toString();
+	const cipher2 = createCipheriv('des-ecb', key2, '').update(TO_ENCODE, 'utf8', 'hex').toString();
+	return (cipher1 + cipher2).toUpperCase();
+}
