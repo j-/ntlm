541e5240d9890766bc4aeb294c74970b814b2a74
j- <j@skeoh.com>
2018-05-31 11:00:10 +1000

    Clean up NTLM module

--- src/ntlm.ts
+++ src/ntlm.ts
@@ -4,15 +4,28 @@ import { hex as md4 } from 'js-md4';
 
 const TO_ENCODE = 'KGS!@#$%';
 
-function passwordToKey (password: Uint8Array, key: Uint8Array) {
-	key[0] =                        (password[0] >> 0);
-	key[1] = ((password[0]) << 7) | (password[1] >> 1);
-	key[2] = ((password[1]) << 6) | (password[2] >> 2);
-	key[3] = ((password[2]) << 5) | (password[3] >> 3);
-	key[4] = ((password[3]) << 4) | (password[4] >> 4);
-	key[5] = ((password[4]) << 3) | (password[5] >> 5);
-	key[6] = ((password[5]) << 2) | (password[6] >> 6);
-	key[7] = ((password[6]) << 1);
+/**
+ * Populate a key buffer from a password buffer for LM hash generation.
+ *
+ * @param password Password buffer to read from
+ * @param key Key buffer to write to
+ * @param i Optional offset index for password buffer
+ * @param j Optional offset index for key buffer
+ */
+function passwordToKey (
+	password: Uint8Array,
+	key: Uint8Array,
+	i: number = 0,
+	j: number = 0,
+) {
+	key[j + 0] =                            (password[i + 0] >> 0);
+	key[j + 1] = ((password[i + 0]) << 7) | (password[i + 1] >> 1);
+	key[j + 2] = ((password[i + 1]) << 6) | (password[i + 2] >> 2);
+	key[j + 3] = ((password[i + 2]) << 5) | (password[i + 3] >> 3);
+	key[j + 4] = ((password[i + 3]) << 4) | (password[i + 4] >> 4);
+	key[j + 5] = ((password[i + 4]) << 3) | (password[i + 5] >> 5);
+	key[j + 6] = ((password[i + 5]) << 2) | (password[i + 6] >> 6);
+	key[j + 7] = ((password[i + 6]) << 1);
 }
 
 export function calculatePassNT (password: string) {
@@ -23,9 +36,9 @@ export function calculatePassLM (password: string) {
 	const buffLM = Buffer.alloc(14);
 	buffLM.write(password.toUpperCase());
 	const key1 = Buffer.alloc(8);
-	passwordToKey(buffLM.slice(0, 7), key1);
 	const key2 = Buffer.alloc(8);
-	passwordToKey(buffLM.slice(7, 14), key2);
+	passwordToKey(buffLM, key1, 0);
+	passwordToKey(buffLM, key2, 7);
 	const cipher1 = createCipheriv('des-ecb', key1, '').update(TO_ENCODE, 'utf8', 'hex').toString();
 	const cipher2 = createCipheriv('des-ecb', key2, '').update(TO_ENCODE, 'utf8', 'hex').toString();
 	return (cipher1 + cipher2).toUpperCase();
